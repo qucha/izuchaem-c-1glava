85680e044f9ef2c698c80a2f65af30f35d7fc100
Qucha <Qucha@RACHELQUCHA>
2021-10-18 20:51:43 +0300

    дорогой калькулятор пример

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,11 +1,18 @@
-#include <iostream>
+
+#include <iostream> 
+
 using namespace std;
 
-int main( )
+int main()
+
 {
-
-	cout << "game over" <<  ::endl;
+	cout << "7 + 3 = " << 7 + 3 << endl;
+	cout << "7 - 3 = " << 7 - 3 << endl;
+	cout << "7 * 3 = " << 7 * 3 << endl;
+	cout << "7 ! 3 = " << 7 / 3 << endl;
+	cout << "7.0 / 3.0 = " << 7.0 / 3.0 << endl;
+	cout << "7 % 3 = " << 7 % 3 << endl;
+	cout << "7 + 3 * 5 = " << 7 + 3 * 5 << endl;
+	cout << "(7 + 3) * 5 = " << (7 + 3) * 5 << endl;
 	return 0;
-
 }
-
